cf59e6a44f5f098a472a0a09ff414fc623194aa4
chervanchukroma bc2024-4 <chervanchukroma@email.com>
2024-10-12 13:29:34 +0300

    Part 1: Implemented basic server with CLI parameters

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Command } = require('commander'); // ќдин раз оголошуЇмо 'Command'
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const program = new Command();
+
+program
+    .requiredOption('-h, --host <host>', 'server host')
+    .requiredOption('-p, --port <port>', 'server port')
+    .requiredOption('-c, --cache <cache>', 'cache directory');
+
+program.parse(process.argv);
+const options = program.opts();
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Server is running\n');
+});
+
+server.listen(options.port, options.host, () => {
+    console.log(`Server running at http://${options.host}:${options.port}/`);
+});
